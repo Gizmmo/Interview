f963c0ac80461c907e3a958ca3fa26e3efeecd55
Travis Scott <travisscott301@gmail.com>
2014-11-20 22:15:11 -0600

    Created the facePlayer Script and added it to the secretary class

---
+++ Assets/Scripts/FacePlayer.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FacePlayer : MonoBehaviour {
+
+	public GameObject target;
+	public Vector2 lookAtPlayerDistance;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector2 spaceBetween = target.transform.position - transform.position;
+		if(Mathf.Abs(spaceBetween.x) <= lookAtPlayerDistance.x && Mathf.Abs(spaceBetween.y) <= lookAtPlayerDistance.y){
+			Quaternion rotation = Quaternion.LookRotation(spaceBetween, transform.TransformDirection(Vector3.up));
+			transform.rotation = new Quaternion(0, 0, rotation.z, rotation.w);
+		}
+	}
+}
